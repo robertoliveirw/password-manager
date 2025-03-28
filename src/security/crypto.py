0a8838ee772ee541360d95975e86240bcf609abd
Roberto <oliveirarobertw@gmail.com>
2025-03-27 22:30:50 -0300

    string parse

--- src/security/crypto.py
+++ src/security/crypto.py
@@ -1,8 +1,9 @@
 import cryptocode
 
 def encrypt_data(data, password='12345'):
-    return cryptocode.encrypt(data, password)
+    encrypted_data = data
+    return cryptocode.encrypt(encrypted_data, password)
 
-def  decryppt_data(encrypted_data, password='12345'):
-    dencrypted_data = encrypted_data
-    return cryptocode.decrypt(dencrypted_data, password)
+def decrypt_data(encrypted_data, password='12345'):
+    decrypted_data = encrypted_data
+    return cryptocode.decrypt(decrypted_data, password)
--- src/utils/string_parser.py
+++ src/utils/string_parser.py
@@ -1,2 +1,15 @@
-def string_parser(texto):
-    #add implementação atual
+from security.crypto import encrypt_data, decrypt_data
+
+def tratar_string(texto):
+    palavras = texto.split()
+    
+    aplicacao = palavras[0] if len(palavras) > 0 else ""
+    usuario = palavras[1] if len(palavras) > 1 else ""
+    email = palavras[2] if len(palavras) > 2 else ""
+    senha = palavras[3] if len(palavras) > 3 else ""
+    
+    encripted_usuario = encrypt_data(usuario)
+    encripted_email = encrypt_data(email)
+    encripted_senha = encrypt_data(senha)
+
+    return aplicacao, encripted_usuario, encripted_email, encripted_senha
